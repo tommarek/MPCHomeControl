b5e730c3bf7ec158c2f59ad8560f2ce47515e3d9
Kuba Marek <blue.cube@seznam.cz>
2022-10-26 13:20:01 +0200

    Separated "as loaded" model and the final one, added adjanced zones
    Also fixing tests from the previous PR

--- MPCHomeControl/src/model.rs
+++ MPCHomeControl/src/model.rs
@@ -6,9 +6,12 @@ use std::iter::once;
 use json5;
 use serde::Deserialize;
 use itertools::chain;
-use uom::si::f32::*;
+use uom::si::f64::{
+    Ratio, Length, Area, Volume,
+    MassDensity, ThermalConductivity, SpecificHeatCapacity, HeatTransfer
+};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 pub struct Model {
     pub materials: HashMap<String, Material>,
     pub boundary_types: HashMap<String, BoundaryType>,
@@ -19,12 +22,10 @@ pub struct Model {
 impl Model {
     pub fn load<P: AsRef<Path>>(path: P) -> anyhow::Result<Self> {
         let string = fs::read_to_string(path)?;
-        let mut loaded = json5::from_str::<Self>(&string)?;
-
-        loaded.verify()?;
-        loaded.expand_sub_boundaries();
-
-        Ok(loaded)
+        let loaded: ModelTmp = json5::from_str(&string)?;
+        let converted: Model = loaded.into();
+        converted.verify()?;
+        Ok(converted)
     }
 
     fn verify(&self) -> anyhow::Result<()> {
@@ -38,12 +39,22 @@ impl Model {
 
         Ok(())
     }
+}
 
-    fn expand_sub_boundaries(&mut self) {
-        self.boundaries = self.boundaries
-            .iter()
-            .flat_map(|boundary| boundary.expanded_sub_boundaries())
-            .collect();
+impl From<ModelTmp> for Model {
+    fn from(value: ModelTmp) -> Self {
+        Model {
+            materials: value.materials,
+            boundary_types: value.boundary_types,
+            zones: value.zones
+                .into_iter()
+                .flat_map(|(zone_name, zone)| zone.expanded_adjanced_zones(zone_name))
+                .collect(),
+            boundaries: value.boundaries
+                .into_iter()
+                .flat_map(|boundary| boundary.expanded_sub_boundaries())
+                .collect(),
+        }
     }
 }
 
@@ -91,48 +102,14 @@ pub struct BoundaryLayer {
     pub thickness: Length,
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
-#[serde(untagged)]
-pub enum Zone {
-     Inner {
-         volume: Volume,
-         #[serde(default)]
-         uncontrollable: bool
-     },
-     Outer,
-}
-
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Boundary {
-    boundary_type: String,
-    zones: [String; 2],
-    area: Area,
-    #[serde(default)]
-    sub_boundaries: Vec<SubBoundary>,
+    pub boundary_type: String,
+    pub zones: [String; 2],
+    pub area: Area,
 }
 
 impl Boundary {
-    /// Convert a boundary to an iterator of boundaries with expanded sub boundaries.
-    fn expanded_sub_boundaries<'a>(&'a self) -> impl Iterator<Item = Self> + 'a {
-        let remaining_area = self.area - self.sub_boundaries.iter().map(|x| x.area).sum::<Area>();
-        chain!(
-            self.sub_boundaries
-                .iter()
-                .map(|sub_boundary| Boundary {
-                    boundary_type: sub_boundary.boundary_type.clone(),
-                    zones: self.zones.clone(),
-                    area: sub_boundary.area,
-                    sub_boundaries: Vec::new()
-                }),
-            once(Boundary {
-                boundary_type: self.boundary_type.clone(),
-                zones: self.zones.clone(),
-                area: remaining_area,
-                sub_boundaries: Vec::new()
-            })
-        )
-    }
-
     fn verify_references(&self, model: &Model) -> anyhow::Result<()> {
         if model.boundary_types.get(&self.boundary_type).is_none() {
             anyhow::bail!(
@@ -153,9 +130,95 @@ impl Boundary {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Zone {
+    Inner {
+        volume: Volume,
+    },
+    Outer,
+}
+
+#[derive(Debug, Deserialize)]
+struct ModelTmp {
+    materials: HashMap<String, Material>,
+    boundary_types: HashMap<String, BoundaryType>,
+    zones: HashMap<String, ZoneTmp>,
+    boundaries: Vec<BoundaryTmp>,
+}
 
 #[derive(Debug, Deserialize, PartialEq)]
-pub struct SubBoundary {
+#[serde(untagged)]
+enum ZoneTmp {
+    Inner {
+        volume: Volume,
+        #[serde(default)]
+        adjacent_zones: Vec<AdjacentZone>
+    },
+    Outer,
+}
+
+impl ZoneTmp {
+    fn expanded_adjanced_zones(self, name: String) -> impl Iterator<Item = (String, Zone)> {
+        let cloned_name = name.clone();
+        let (adjanced, ret) = match self {
+            Self::Inner { volume, adjacent_zones } => (adjacent_zones, Zone::Inner { volume }),
+            Self::Outer => (Vec::new(), Zone::Outer),
+        };
+
+        chain!(
+            adjanced
+                .into_iter()
+                .map(move |adj_zone| (
+                    format!("{}/{}", cloned_name, adj_zone.suffix),
+                    Zone::Inner { volume: Default::default() }
+                )),
+            once((name, ret))
+        )
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct AdjacentZone {
+    suffix: String,
+    boundary_type: String,
+    area: Area,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[cfg_attr(test, derive(Clone))]
+struct BoundaryTmp {
+    boundary_type: String,
+    zones: [String; 2],
+    area: Area,
+    #[serde(default)]
+    sub_boundaries: Vec<SubBoundary>,
+}
+
+impl BoundaryTmp {
+    /// Convert a boundary to an iterator of boundaries with expanded sub boundaries.
+    fn expanded_sub_boundaries(self) -> impl Iterator<Item = Boundary> {
+        let remaining_area = self.area - self.sub_boundaries.iter().map(|x| x.area).sum::<Area>();
+        let cloned_zones = self.zones.clone();
+        chain!(
+            self.sub_boundaries
+                .into_iter()
+                .map(move |sub_boundary| Boundary {
+                    boundary_type: sub_boundary.boundary_type,
+                    zones: cloned_zones.clone(),
+                    area: sub_boundary.area,
+                }),
+            once(Boundary {
+                boundary_type: self.boundary_type,
+                zones: self.zones,
+                area: remaining_area,
+            })
+        )
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[cfg_attr(test, derive(Clone))]
+struct SubBoundary {
     boundary_type: String,
     area: Area,
 }
@@ -235,6 +298,7 @@ let entrance_window = DoorWindow {
 mod tests {
     use super::*;
     use test_strategy::proptest;
+    use uom::si::area::square_meter;
     //use more_asserts::*;
 
     /// Test Boundary::expanded_sub_boundaries when no actual expansion is necessary
@@ -242,25 +306,28 @@ mod tests {
     fn expanded_sub_boundaries_no_expansion(
         boundary_type: String,
         zones: [String; 2],
-        area: Area,
+        area: f64,
     ) {
-        let b = Boundary { boundary_type, zones, area, sub_boundaries: Vec::new() };
-        let expanded: Vec<_> = b.expanded_sub_boundaries().collect();
+        let area = Area::new::<square_meter>(area);
+        let b = BoundaryTmp { boundary_type, zones, area, sub_boundaries: Vec::new() };
+        let expanded: Vec<_> = b.clone().expanded_sub_boundaries().collect();
         assert_eq!(expanded.len(), 1);
-        assert_eq!(expanded[0], b);
+        assert_eq!(expanded[0].boundary_type, b.boundary_type);
+        assert_eq!(expanded[0].zones, b.zones);
+        assert_eq!(expanded[0].area, b.area);
     }
 
     /// Test Boundary::expanded_sub_boundaries on a hand crafted example
     #[test]
     fn expanded_sub_boundaries_example() {
-        let b = Boundary {
+        let b = BoundaryTmp {
             boundary_type: "t1".to_string(),
             zones: ["z1".to_string(), "z2".to_string()],
-            area: 100.0,
+            area: Area::new::<square_meter>(100.0),
             sub_boundaries: vec![
-                SubBoundary { boundary_type: "t2".to_string(), area: 3.0 },
-                SubBoundary { boundary_type: "t3".to_string(), area: 1.0 },
-                SubBoundary { boundary_type: "t4".to_string(), area: 4.0 },
+                SubBoundary { boundary_type: "t2".to_string(), area: Area::new::<square_meter>(3.0) },
+                SubBoundary { boundary_type: "t3".to_string(), area: Area::new::<square_meter>(1.0) },
+                SubBoundary { boundary_type: "t4".to_string(), area: Area::new::<square_meter>(4.0) },
             ]
         };
         let expanded: Vec<_> = b.expanded_sub_boundaries().collect();
@@ -269,94 +336,23 @@ mod tests {
             Boundary {
                 boundary_type: "t2".to_string(),
                 zones: ["z1".to_string(), "z2".to_string()],
-                area: 3.0,
-                sub_boundaries: vec![]
+                area: Area::new::<square_meter>(3.0),
             },
             Boundary {
                 boundary_type: "t3".to_string(),
                 zones: ["z1".to_string(), "z2".to_string()],
-                area: 1.0,
-                sub_boundaries: vec![]
+                area: Area::new::<square_meter>(1.0),
             },
             Boundary {
                 boundary_type: "t4".to_string(),
                 zones: ["z1".to_string(), "z2".to_string()],
-                area: 4.0,
-                sub_boundaries: vec![]
+                area: Area::new::<square_meter>(4.0),
             },
             Boundary {
                 boundary_type: "t1".to_string(),
                 zones: ["z1".to_string(), "z2".to_string()],
-                area: 92.0,
-                sub_boundaries: vec![]
+                area: Area::new::<square_meter>(92.0),
             },
         ]);
-    }
-
-    #[test]
-    fn test_model_expand_sub_boundaries() {
-        let mut model = Model {
-            materials: HashMap::new(),
-            boundary_types: HashMap::new(),
-            zones: HashMap::new(),
-            boundaries: vec![
-                Boundary {
-                    boundary_type: "t1".to_string(),
-                    zones: ["z1".to_string(), "z2".to_string()],
-                    area: 100.0,
-                    sub_boundaries: vec![
-                        SubBoundary { boundary_type: "t2".to_string(), area: 3.0 },
-                        SubBoundary { boundary_type: "t3".to_string(), area: 1.0 },
-                        SubBoundary { boundary_type: "t4".to_string(), area: 4.0 },
-                    ]
-                },
-                Boundary {
-                    boundary_type: "t5".to_string(),
-                    zones: ["z3".to_string(), "z4".to_string()],
-                    area: 1.0,
-                    sub_boundaries: vec![]
-                },
-            ]
-        };
-
-        model.expand_sub_boundaries();
-
-        // TODO: This test is fragile w.r.t. output boundary order.
-        assert_eq!(
-            model.boundaries,
-            vec![
-                Boundary {
-                    boundary_type: "t2".to_string(),
-                    zones: ["z1".to_string(), "z2".to_string()],
-                    area: 3.0,
-                    sub_boundaries: vec![]
-                },
-                Boundary {
-                    boundary_type: "t3".to_string(),
-                    zones: ["z1".to_string(), "z2".to_string()],
-                    area: 1.0,
-                    sub_boundaries: vec![]
-                },
-                Boundary {
-                    boundary_type: "t4".to_string(),
-                    zones: ["z1".to_string(), "z2".to_string()],
-                    area: 4.0,
-                    sub_boundaries: vec![]
-                },
-                Boundary {
-                    boundary_type: "t1".to_string(),
-                    zones: ["z1".to_string(), "z2".to_string()],
-                    area: 92.0,
-                    sub_boundaries: vec![]
-                },
-                Boundary {
-                    boundary_type: "t5".to_string(),
-                    zones: ["z3".to_string(), "z4".to_string()],
-                    area: 1.0,
-                    sub_boundaries: vec![]
-                },
-            ]
-        );
-
     }
 }
