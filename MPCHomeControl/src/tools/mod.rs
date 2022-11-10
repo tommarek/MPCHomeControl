6d3f340c89afd1949857dc164d5c894829b1c333
Tom Marek <tm.tomas.marek@gmail.com>
2022-11-10 16:54:13 +0100

    fix comments, typos

--- MPCHomeControl/src/tools/mod.rs
+++ MPCHomeControl/src/tools/mod.rs
@@ -9,13 +9,19 @@ use uom::si::{
     ratio::ratio,
 };
 
-/**
- * Get effective iluminated area. This can be used to get the norm vector of a wall or sun vector
- *
- * @param azimuth Azimuth of the sun or the direction of wall normal vector
- * @param zenith_angle Zenith angle of the sun or the angle between wall normal
- *                     vector and the vertical vector
- */
+/// Get three dimensional Vector from azimuth and zenith angle/wall normal.
+/// This can be used to get the norm vector of a wall or sun vector
+///
+/// Output vector coordinate system is following:
+/// - north -> positive x axis / south -> negative x axis
+/// - east -> positive y axis / west -> negative y axis
+/// - z: up
+///
+/// # Arguments
+/// * `azimuth` - angle between north and the wall normal / sun vector
+/// * `zenith_angle` - angle between the wall normal / sun vector and the z axis
+/// # Returns
+/// * `Vector3<f64>` - three dimensional vector
 pub fn get_vector_from_angles(azimuth: Angle, zenith_angle: Angle) -> Vector3<f64> {
     let x = zenith_angle.sin().get::<ratio>() * azimuth.cos().get::<ratio>();
     let y = zenith_angle.sin().get::<ratio>() * azimuth.sin().get::<ratio>();
@@ -23,7 +29,14 @@ pub fn get_vector_from_angles(azimuth: Angle, zenith_angle: Angle) -> Vector3<f6
     Vector3::new(x, y, z).normalize()
 }
 
-fn get_ilumination_coefficient(sun_vector: &Vector3<f64>, surface_normal: &Vector3<f64>) -> f64 {
+/// Get a coefficient value used for calculating effectively illuminated area.
+///
+/// # Arguments
+/// * `sun_vector` - normalzied vector pointing to the sun
+/// * `surface_normal` - normalized vector of a wall normal
+/// # Returns
+/// * `f64` - coefficient value [0-1]
+fn get_illumination_coefficient(sun_vector: &Vector3<f64>, surface_normal: &Vector3<f64>) -> f64 {
     let cos_theta = sun_vector.dot(surface_normal);
     if cos_theta < 0.0 {
         0.0
@@ -31,7 +44,7 @@ fn get_ilumination_coefficient(sun_vector: &Vector3<f64>, surface_normal: &Vecto
         cos_theta
     }
 }
-pub fn get_effective_iluminated_area(
+pub fn get_effective_illuminated_area(
     lat: f64,
     lon: f64,
     surface_normal: Vector3<f64>,
@@ -45,7 +58,7 @@ pub fn get_effective_iluminated_area(
     );
     let surface_normal = surface_normal.normalize();
 
-    let cos_theta = get_ilumination_coefficient(&sun_vector, &surface_normal);
+    let cos_theta = get_illumination_coefficient(&sun_vector, &surface_normal);
     let area: Area = Area::new::<square_meter>(area.get::<square_meter>() * cos_theta);
     anyhow::Ok(area)
 }
@@ -65,18 +78,18 @@ mod tests {
     }
 
     #[test]
-    fn test_get_ilumination_coef_direct_sunlight() {
+    fn test_get_illumination_coef_direct_sunlight() {
         let sun_vector = Vector3::new(0.0, 0.0, 1.0);
         let surface_normal = Vector3::new(0.0, 0.0, 1.0);
-        let coef = super::get_ilumination_coefficient(&sun_vector, &surface_normal);
+        let coef = super::get_illumination_coefficient(&sun_vector, &surface_normal);
         assert_approx_eq_eps!(1.0, coef, 0.1);
     }
 
     #[test]
-    fn test_get_ilumination_coef_45deg_sunlight() {
+    fn test_get_illumination_coef_45deg_sunlight() {
         let sun_vector = Vector3::new(0.0, 0.0, 1.0);
         let surface_normal = Vector3::new(0.7071067811865475, 0.0, 0.7071067811865476);
-        let coef = super::get_ilumination_coefficient(&sun_vector, &surface_normal);
+        let coef = super::get_illumination_coefficient(&sun_vector, &surface_normal);
         assert_approx_eq_eps!(0.7071, coef, 0.1);
     }
 }
